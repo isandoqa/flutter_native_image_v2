f3ddd8bbd729f9c0743e143a80e75d22bd4d7c36
ishaqsandoqah <i.sandoqh@t2.sa>
2025-04-21 17:17:46 +0300

    update android plugin

--- android/src/main/java/com/example/flutternativeimage/FlutterNativeImagePlugin.java
+++ android/src/main/java/com/example/flutternativeimage/FlutterNativeImagePlugin.java
@@ -29,7 +29,7 @@ public class FlutterNativeImagePlugin implements FlutterPlugin {
 
   private void teardownChannel() {
     if (methodCallHandler != null) {
-      methodCallHandler.cleanup();
+      //methodCallHandler.cleanup();
       methodCallHandler = null;
     }
     if (channel != null) {
